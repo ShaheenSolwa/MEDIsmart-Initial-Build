30bf9ac231570282b5766b26b6c550b113642217
ShaheenSolwa <56385860+ShaheenSolwa@users.noreply.github.com>
2021-10-20 19:09:12 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 ########################################################################################
 ######################          Import packages      ###################################
 ########################################################################################
-from flask import Blueprint, render_template, flash
+from flask import redirect,Blueprint, render_template, url_for, flash, request
 from flask_login import login_required, current_user
 from __init__ import create_app, db
+from werkzeug.utils import secure_filename
+import os
 
 ########################################################################################
 # our main blueprint
 main = Blueprint('main', __name__)
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @main.route('/') # home page that return 'index'
 def index():
@@ -25,6 +32,35 @@ def Pricing():
 @main.route('/contribute')
 def Contribute():
     return render_template('contribute.html')
+
+@main.route('/upload')
+def upload_form():
+    return render_template('upload.html')
+
+@main.route('/upload', methods=['POST'])
+def uploaded():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No image selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # print('upload_image filename: ' + filename)
+            flash('Image successfully uploaded and displayed below')
+            return render_template('upload.html', filename=filename)
+        else:
+            flash('Allowed image types are -> png, jpg, jpeg, gif')
+            return redirect(request.url)
+
+@main.route('/upload/display/<filename>')
+def display_image(filename):
+    # print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 @main.route('/predict')
 def predict():
